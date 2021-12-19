56ec7cdcdc299741d2f61dc81eba49b0e1c48bb3
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2021-12-19 21:48:16 +0300

    Собрал приложение

--- app/src/main/java/com/proect/qrgenerator/MainActivity.kt
+++ app/src/main/java/com/proect/qrgenerator/MainActivity.kt
@@ -2,10 +2,34 @@ package com.proect.qrgenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import com.google.zxing.WriterException
 
 class MainActivity : AppCompatActivity() {
+    var im: ImageView? = null
+    var bGenerate: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        im = findViewById(R.id.imageView)
+        bGenerate = findViewById(R.id.button)
+        bGenerate?.setOnClickListener {
+            generateQrCode("QR код сгенерирован")
+        }
+    }
+
+    private fun generateQrCode(text: String){
+        val qrGeneretor = QRGEncoder(text, null, QRGContents.Type.TEXT, 500)
+        try {
+
+            val bMap = qrGeneretor.encodeAsBitmap()
+            im?.setImageBitmap(bMap)
+        } catch (e: WriterException){
+
+        }
     }
 }
